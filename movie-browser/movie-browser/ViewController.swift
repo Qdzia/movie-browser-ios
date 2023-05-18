494ec18bd2b733b587de9c3d6b6e217b03e069fb
Łukasz Kudzia <qdzia.work@gmail.com>
2023-05-18 12:11:07 +0200

    Add SnapKit

--- movie-browser/movie-browser/ViewController.swift
+++ movie-browser/movie-browser/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let homeLabel = UILabel(frame: .zero)
+        homeLabel.text = "Home Screen"
+        homeLabel.textAlignment = .center
+        view.addSubview(homeLabel)
+        homeLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
-
-
 }
 
