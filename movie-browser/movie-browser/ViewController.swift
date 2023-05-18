42d8dc0210cc5ff78708b2182b576d3420836272
Łukasz Kudzia <qdzia.work@gmail.com>
2023-05-18 12:05:34 +0200

    Set up empty project

---
+++ movie-browser/movie-browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-browser
+//
+//  Created by Łukasz Kudzia on 18/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
